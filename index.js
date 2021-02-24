d2fce4b77ffd2a4b91a3da052231a4afcc5f4b34
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-02-23 20:38:18 -0500

    Add inquirer

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
 
     console.log("doing ok so far");
