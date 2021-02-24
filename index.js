e2ff42eb7f8980821568cff950b8fb17d54aed27
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-02-23 19:49:09 -0500

    Create classes from Employee for Engineer, Intern, and Mgr

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const Employee = require(".lib/Employee.js");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
     console.log("doing ok so far");
