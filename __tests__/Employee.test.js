f306fb161e5f29557cdc53667c962e49344a4f94
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-02-27 19:13:58 -0500

    Write employee test code and test--success

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require("../lib/Employee");
 
-test("can set a employee name", () => {
+test("can set an employee name", () => {
     const name = "Rani";
     const employee = new Employee(name);
 
@@ -8,9 +8,49 @@ test("can set a employee name", () => {
 
 });
 
-test("can set employee email", () => {
-    const email = "joesmith@gmail.com";
-    const employee = new Employee("Rani", 1, email);
+test("can set an employee ID", () => {
+    const id = "100";
+    const employee = new Employee("Joe Smith", id, "joesmith@gmail.com");
 
-    expect (employee.getEmail()).toBe("joesmith@gmail.com");
+    expect(employee.id).toBe(id);
+
+});
+
+test("can set an employee email", () => {
+    const email = "joesmith@gmail.com";
+    const employee = new Employee("Joe Smith", "12", email);
+
+    expect(employee.email).toBe(email);
+
+});
+
+test("can get employee Name with getName()", () => {
+    const name = "Joe Smith";
+    const employee = new Employee(name, "12", "joesmith@gmail.com");
+
+    expect(employee.getName()).toBe(name);
+
+});
+
+test("can get employee Id with getId()", () => {
+    const id = "100";
+    const employee = new Employee("Joe Smith", id, "joesmith@gmail.com");
+
+    expect(employee.getId()).toBe(id);
+
+});
+
+
+test("can get employee email with getEmail()", () => {
+    const email = "joesmith@gmail.com";
+    const employee = new Employee("Joe Smith", "1", email);
+
+    expect (employee.getEmail()).toBe(email);
+})
+
+test("getRole() returns Employee", () => {
+    const role = "Employee";
+    const employee = new Employee("Joe Smith", "1", "joesmith@gmail.com");
+
+    expect (employee.getRole()).toBe(role);
 })
