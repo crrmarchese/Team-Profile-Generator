2a4635713e8d3698c6ec54e9e6cd35bb2067f354
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-02-27 19:25:48 -0500

    Write engineer test code-passes test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,24 @@
 const Engineer = require("../lib/Engineer");
+
+test("can set GitHub account", () => {
+    const github = "githubuseraccount";
+    const employee = new Engineer("Joe Smith", "1", "joesmith@gmail.com", github);
+
+    expect(employee.github).toBe(github);
+
+});
+
+test("can get Github accout with getGithub()", () => {
+    const github = "githubuseraccount";
+    const employee = new Engineer("Joe Smith", "1", "joesmith@gmail.com", github);
+
+    expect(employee.getGithub()).toBe(github);
+
+});
+
+test("getRole() returns Engineer", () => {
+    const role = "Engineer";
+    const employee = new Engineer("Joe Smith", "1", "joesmith@gmail.com");
+
+    expect (employee.getRole()).toBe(role);
+})
